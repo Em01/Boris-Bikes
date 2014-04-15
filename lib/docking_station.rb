659eb89b3daa727792c8daeb4ddd5c76e544f0da
Emma <emma_williams1@hotmail.co.uk>
2014-04-15 14:28:11 +0100

    Refactored docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
 class DockingStation
+  DEFAULT_CAPACITY = 10
 
-  def initialize
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
   end
 
@@ -9,7 +11,18 @@ class DockingStation
   end
 
   def dock(bike)
+    #if the capacity is reached, raise an exception
+    raise "Station is full" if full?
     @bikes << bike 
+  end
+
+  def release(bike)
+    @bikes.delete(bike)
+  end
+
+  def full?
+    bike_count == @capacity 
+  
   end
 
 end
